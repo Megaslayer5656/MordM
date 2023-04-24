a3396f0337c3df2e6c0823ff7b516196a9a0e257
Megaslayer5656 <93740323+Megaslayer5656@users.noreply.github.com>
2023-04-24 19:53:10 -0400

    Update init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,6 +1,9 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 AddCSLuaFile("config.lua")
+AddCSLuaFile("consolecmds.lua")
+AddCSLuaFile("cl_hud.lua")
 
 include("shared.lua")
 include("config.lua")
+include("consolecmds.lua")
