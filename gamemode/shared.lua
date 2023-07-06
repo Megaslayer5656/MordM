69711b0cad08f9850f56a521d62ddd5b8d4f14ee
Megaslayer5656 <justinbex08@gmail.com>
2023-07-06 00:39:14 -0400

    Added menu framework
    Yippee I finally got it working

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,9 +1,9 @@
 GM.Name = "MordM"
 GM.Author = "Megaslayer_"
 GM.Email = "N/A"
-GM.Website = "N/A"
+GM.Website = "https://github.com/Megaslayer5656"
 function GM:Initialize()
-    
+    print("Starting MordM")
 end
 function GM:PlayerNoClip()
     return false
@@ -15,3 +15,20 @@ function GM:PlayerSpawn(ply)
     ply:SetupHands()
     print("Weapon Given (spawned)")
 end
+function Mainmenuspec(ply)
+    ply:Spectate(6)
+end
+function Unspec(ply)
+    ply:UnSpectate()
+end
+function Remitems(ply)
+    ply:RemoveAllItems()
+end
+function ItemMenuClose(ply)
+    local randwep = math.random(1,40)
+    ply:Give(Weapon[randwep])
+end
+concommand.Add("Mainmenuspec", Mainmenuspec)-- IMPORTANT: These commands for the menu are just for current testing, and are not going to be a thing later
+concommand.Add("Unspec", Unspec)
+concommand.Add("Remitems", Remitems)
+concommand.Add("Itemmenuclose", ItemMenuClose)
